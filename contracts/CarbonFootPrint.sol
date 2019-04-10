856afb60fcd4cbeebd56d6e90caf0dabafbdf758
xicosantos98 <fjrs.ipvc@gmail.com>
2019-04-10 19:16:27 +0100

    Métodos do criação feitos

--- contracts/CarbonFootPrint.sol
+++ contracts/CarbonFootPrint.sol
@@ -21,7 +21,7 @@ contract CarbonFootPrint{
         uint32 co2eq;
         uint16 exp;
         uint32 idProduct;
-        uint32 idAno;
+        uint32 idYear;
     }
 
     struct ProductQuantity{
@@ -37,6 +37,7 @@ contract CarbonFootPrint{
         uint32 id;
         uint32 co2eq;
         uint16 exp;
+        string name;
         uint32[] products;
         uint32[] m_activities;
         uint32[] m_fixcosts;
@@ -113,28 +114,28 @@ contract CarbonFootPrint{
 
     struct Year{
         uint32 id;
-        string ano;
+        string year;
     }
 
 
     // -- Mappings
     mapping(uint32 => Product) public products;
-    uint32 productsCount;
+    uint32 public productsCount;
 
     mapping(uint32 => MonthlyActivity) public mactivities;
-    uint32 mactivitiesCount;
+    uint32 public mactivitiesCount;
 
     mapping(uint32 => Organization) public organizations;
-    uint32 organizationsCount;
+    uint32 public organizationsCount;
 
     mapping(uint32 => MonthlyFixCost) public mfixcosts;
-    uint32 mfixcostsCount;
+    uint32 public mfixcostsCount;
 
     mapping(uint32 => ProductCost) public productCosts;
-    uint32 productCostsCount;
+    uint32 public productCostsCount;
 
     mapping(uint32 => CostType) public costsTypes;
-    uint32 costsTypesCount;
+    uint32 public costsTypesCount;
 
     mapping(uint32 => Unit) public units;
     uint32 public unitsCount;
@@ -143,13 +144,13 @@ contract CarbonFootPrint{
     address[] public arrayUsers;
 
     mapping(uint32 => ProductFootprint) public productFootPrints;
-    uint32 pfootPrintCount;
+    uint32 public pfootPrintCount;
 
     mapping(uint32 => ProductQuantity) public productsQuantities;
-    uint32 productsQuantitiesCount;
+    uint32 public productsQuantitiesCount;
 
     mapping(uint32 => Year) public yearly;
-    uint32 yearliesCount;
+    uint32 public yearliesCount;
 
     event registUserEvent (
         address indexed _candidateAddress
@@ -168,6 +169,16 @@ contract CarbonFootPrint{
 
     function getUsersCount() public view returns(uint count) {
         return arrayUsers.length;
+    }
+
+    // -- GETTERS -> organization
+    function getOrgProducts(uint32 _org) public view returns(uint32[] memory prods){
+        return organizations[_org].products;
+    }
+
+    // -- GETTERS -> products
+    function getFootPrintsProd(uint32 _prod) public view returns(uint32[] memory footprints){
+        return products[_prod].productFootPrints;
     }   
 
 
@@ -193,14 +204,92 @@ contract CarbonFootPrint{
 
     function addUnit(string memory _measure, string memory _initials, uint32 _base, uint32 _exp, uint32 _idUnit,bool _negative) private{
         
-        require(users[msg.sender].tipo == 0, "Necessita ter permissões 'Administrador'");
+        require(users[msg.sender].tipo == 0, "You need to have admin permissions");
 
         unitsCount++;
         Unit(unitsCount, _measure, _initials, _base, _exp, _idUnit, _negative);
         units[unitsCount] = Unit(unitsCount, _measure, _initials, _base, _exp, _idUnit, _negative);
     }
 
+    function addOrganization(string memory _name, uint32[] memory _products, uint32[] memory _m_activities, uint32[] memory _m_fixcosts) public {
+        require(users[msg.sender].tipo == 0, "You need to have admin permissions");
 
+        organizationsCount++;
+        organizations[organizationsCount] = Organization(organizationsCount, 0, 0, _name, _products, _m_activities, _m_fixcosts); 
+    }
+
+    function addProduct(string memory _name, string memory _description, 
+        bool _intermediate, uint32 _org, uint32 _unit, uint32[] memory _footPrints) public{
+        
+        //require(users[msg.sender].idOrganization == _org, "You need to belong to the organization");
+
+        productsCount++;
+        products[productsCount] = Product(productsCount, _name, _description, units[_unit].initials, 0, 0,
+            _intermediate, _org, _unit, _footPrints);
+        organizations[_org].products.push(productsCount);
+
+    }
+
+    function addYear(string memory _year) public {
+        require(users[msg.sender].tipo == 0, "You need to have admin permissions");        
+        yearliesCount++;
+        yearly[yearliesCount] = Year(yearliesCount, _year);
+    }
+
+    function addFootPrintProd(uint32 _co2eq, uint16 _exp, uint32 _idProd, uint32 _idYear) public {
+        
+        //require(users[msg.sender].idOrganization == products[_idProd].idOrganization, "The product doesnt belong to your organization");        
+
+        pfootPrintCount++;
+        productFootPrints[pfootPrintCount] = ProductFootprint(pfootPrintCount, _co2eq, _exp, _idProd, _idYear);
+        products[_idProd].productFootPrints.push(pfootPrintCount);
+    }
+
+    function updateCO2Prod(uint32 _id, uint32 _co2eq, uint16 _exp) public {
+        products[_id].co2eq = _co2eq;
+        products[_id].exp = _exp;
+    }
+
+    function addCostType(uint32 _co2, uint16 _exp, string memory _desc, uint32 _idUnit, 
+        uint32[] memory _fixCosts, uint32[] memory _p_costs) public {
+
+        //require(users[msg.sender].tipo == 0 || users[msg.sender].tipo == 1, "You need to have admin/org admin permissions");        
+        
+
+        costsTypesCount++;
+        costsTypes[costsTypesCount] = CostType(costsTypesCount, _co2, _exp, _desc, _idUnit, _fixCosts, _p_costs);
+    }
+
+    function addMonthlyFixCost(uint32 _co2eq, uint16 _exp, string memory _desc, uint32 _qtd, 
+        string memory _month,uint32 _idCost,uint32 _idOrg, uint32 _idYear) public{
+
+        //require(users[msg.sender].idOrganization == _idOrg, "You need to belong to the organization");        
+
+        mfixcostsCount++;
+        mfixcosts[mfixcostsCount] = MonthlyFixCost(mfixcostsCount, _co2eq, _exp, _desc, _qtd, _month, _idCost, _idOrg, _idYear);
+        costsTypes[_idCost].m_fixcosts.push(mfixcostsCount);
+        organizations[_idOrg].m_fixcosts.push(mfixcostsCount);
+    }
+
+    function addMonthlyActivity(string memory _desc, uint32 _co2eq, uint32 _qtd, uint16 _exp, string memory _month,
+        uint32[] memory _prodQuantities, uint32 _output, uint32 _idOrg, uint32 _idUnit, uint32 _idYear) public{
+        
+        //require(users[msg.sender].idOrganization == _idOrg, "You need to belong to the organization");    
+
+        mactivitiesCount++;
+        mactivities[mactivitiesCount] = MonthlyActivity(mactivitiesCount, _desc, _co2eq, _qtd, _exp, _month, _prodQuantities, _output, _idOrg, _idUnit, _idYear, msg.sender);
+        organizations[_idOrg].m_fixcosts.push(mactivitiesCount);
+    }
+
+
+    function addProdQuantity(uint32 _qtd, uint32 _idPfootPrint, uint32 _m_activity) public{
+        productsQuantitiesCount++;
+        productsQuantities[productsQuantitiesCount] = ProductQuantity(productsQuantitiesCount, _qtd, _idPfootPrint, _m_activity);
+    }
+
+    function updateMonthlyActivity(uint32 _idProd, uint32 _idMA) public {
+        mactivities[_idMA].productQuantities.push(_idProd);
+    }
 
 
 
--- test/footprint.js
+++ test/footprint.js
@@ -48,4 +48,53 @@ contract("CarbonFootPrint", function (accounts) {
         assert.equal(await cfootprintInstance.unitsCount(), 4);
     })
 
+    it("allows to create new organization", async function() {
+        var nome = "Lidia e Costa";
+        await cfootprintInstance.addOrganization(nome, [], [], []);
+        var organization = await cfootprintInstance.organizations(1);
+        assert.equal(nome, organization.name);
+    })
+
+    it("allows to create new product", async function(){
+        await cfootprintInstance.addProduct("Tecido", "10 kg de tecido", true, 0, 2, []);
+        assert.equal(1, await cfootprintInstance.productsCount());
+    })
+
+    it("allows to create new year", async function(){
+        await cfootprintInstance.addYear("2019");
+        assert.equal(1, await cfootprintInstance.yearliesCount());
+    })
+
+    it("allows to create new product footprint", async function(){
+        await cfootprintInstance.addProduct("Tecido", "10 kg de tecido", true, 0, 2, []);
+        await cfootprintInstance.addFootPrintProd(324, 3, 1, 0);
+        assert.equal(1, await cfootprintInstance.pfootPrintCount());
+    })
+    
+    it("allows to create new cost type", async function(){
+        await cfootprintInstance.addCostType(353, 2, "Gas flared", 2, [], []);
+        assert.equal(1, await cfootprintInstance.costsTypesCount());
+    })
+    
+    it("allows to create new monthly fix cost", async function(){
+        await cfootprintInstance.addMonthlyFixCost(353, 2, "Electricity", 5000, "April", 0, 0 , 0);
+        assert.equal(1, await cfootprintInstance.mfixcostsCount());
+    })
+
+    it("allows to create new monthly activity", async function(){
+        await cfootprintInstance.addMonthlyActivity("packing", 353, 5000, 3, "April", [], 0, 0, 2, 1);
+        assert.equal(1, await cfootprintInstance.mactivitiesCount());
+    })
+
+    it("allows to create new product quantity", async function(){
+        await cfootprintInstance.addProdQuantity(100, 0, 0);
+        assert.equal(1, await cfootprintInstance.productsQuantitiesCount());
+    })
+
+
+
+
+
+
+
 })
